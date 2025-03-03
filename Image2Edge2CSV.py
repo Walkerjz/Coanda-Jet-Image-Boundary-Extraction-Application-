cc11dbd7d5e138cdd5c4763f713f61dfec1d2c6e
Walkerjz <walkerjz@dukes.jmu.edu>
2025-03-03 16:57:16 -0500

    good comments

--- Image2Edge2CSV.py
+++ Image2Edge2CSV.py
@@ -2,12 +2,30 @@ import cv2
 import time
 import numpy as np
 
-#FilePath is the image you are edge detecting.
+'''
+This program takes Scherien Images of Coanda Jets and extracts the jet boundary using canny edge detection.
+To use this program change the file name and run the program. Adjust the sliders untill the edge detection looks good 
+then press 'q' to exit and save the points as a csv with the same name as the image file.
+
+Adjustable functions:
+-Change directory file path
+-change file name
+-change background image
+-change mask file
+-change bilateral filter
+-change canny edge detection aperture size 
+-The i and bottom values methods determine where the top and bottom pixels that are extracted.
+'''
+
+#FilePath is the directory you are pulling from
 FilePath = 'Data/1800um/'
+#file Name is the image we want to extract the boundary from
 FileName = 'rcphoto1810um21psi_2019.jpg'
-#TarePath is the lowest pressure image.
+#TarePath is a background image.
+# In the absence of a background image use the lowest pressure in a slot width set you are testing
 TareName = 'rcphoto1810um5psi_2019.jpg'
-# some other files ['Data/1800um/Tare1810um10psi.jpg','Data/1800um/Tare1810um15psi.jpg','Data/1800um/Tare1810um20psi.jpg','Data/1800um/Tare1810um21psi.jpg']
+# The mask file was created using ImageJ. It is used here to get rid of random flow phenomena
+#floating around in the fluid flow
 Mask = 'Data/1800um/UniversalMask2.jpg'
 
 #Read images and convert them to a gray scale format so the matrix is 2D not 3D
@@ -15,19 +33,22 @@ File = cv2.cvtColor(cv2.imread(FilePath+FileName), cv2.COLOR_BGR2GRAY)
 Tare = cv2.cvtColor(cv2.imread(FilePath+TareName), cv2.COLOR_BGR2GRAY)
 Mask = cv2.cvtColor(cv2.imread(Mask), cv2.COLOR_BGR2GRAY)
 
+#Subtract the background from the image to boost contrast
 Diff = cv2.subtract(File, Tare)
+# use the mask to get rid of random flow phenomena
+# floating around in the fluid flow
 image = cv2.subtract(Diff,Mask)
 
-#canny thresholds of 0 and 207 work for the psipath files to answer question 3 and a filter d=5
-# d=12 works well for Data/rcphoto2370um30psi_2019.jpg
+
 def apply_canny(image, low_threshold, high_threshold):
-    """Applies Canny edge detection with adjustable thresholds."""
-    # Apply Canny edge detection
-    #filter the image
+    """Applies Canny edge detection and a bilateral filter with adjustable thresholds."""
+    #The bilateral filter filters the image while maintaining edges
+    #sigmacolor shouldn't do much because we are only using one color
+    # you can read more here https://www.geeksforgeeks.org/python-bilateral-filtering/
     filt = cv2.bilateralFilter(image,d=5, sigmaColor=1000, sigmaSpace=75)
-    #apply the canny image detection and return image of the edges found
+    #apply the canny image detection and returns the image of the edges found
     edges = cv2.Canny(filt, low_threshold, high_threshold, apertureSize=5)
-    # return the grayscale image and edges
+    # return the edges
     return edges
 
 def extract_edges(edges):
@@ -37,7 +58,8 @@ def extract_edges(edges):
     edgeY = []
     #start from y = 260 and iterate through rows until row 316. These values are adjustable to avoid unwanted edge artifacts
     i = 260
-    while i<316:
+    bottom = 316
+    while i<bottom:
         # start from the right of the image and iterate across
         j = 605
         while j>=0:
